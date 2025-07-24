4a5eaa1266f09f876a176afffc5bede72e6b3d0d
shourov.foisal <shourov.foisal@technonext.com>
2025-07-24 18:04:16 +0600

    add TreeMap example

--- src/main/java/org/shourov/collectors/GroupingBy.java
+++ src/main/java/org/shourov/collectors/GroupingBy.java
@@ -5,6 +5,7 @@ import org.shourov.Utils;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 /**
@@ -25,6 +26,12 @@ public class GroupingBy {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
         Map<String, List<Integer>> group = numbers.stream().collect(Collectors.groupingBy(elem -> elem % 2 == 0 ? "Even" : "Odd"));
 
+        Utils.printMap(group);
+    }
+
+    public static void groupingWithTreeMap() {
+        List<String> names = Arrays.asList("Alex", "Bob", "Caleb", "Derek", "Evan", "Fred");
+        Map<Integer, List<String>> group = names.stream().collect(Collectors.groupingBy(String::length, TreeMap::new, Collectors.toList()));
         Utils.printMap(group);
     }
 }
